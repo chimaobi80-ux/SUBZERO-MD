835336e79fc872e7ffe5ea2bc0ccf275123659fe
chimaobi80-ux <ebohdivine55@gmail.com>
2025-07-31 18:01:18 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO-MD;;;iB9wjZzI#jwK0L7gcy01WliWkQC0CGW0V4zhsImV7eMpOIgAUMLQ", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;ncVD1SYb#kDIUxoDbLAJBUkjkerfDZiomxX6GNsH9Wg_YEBpbAOo", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "2347015996525", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
